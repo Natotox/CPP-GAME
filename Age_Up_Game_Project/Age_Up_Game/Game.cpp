a7bc874882462027fd5e07a4e035e12522ce3516
Natalie Mills <natomills63@gmail.com>
2023-12-06 19:30:53 -0400

    CREATE PROJECT

---
+++ Age_Up_Game_Project/Age_Up_Game/Game.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void Update_Time(string task){
+
+    int hour, minute;
+
+    if(task=="class"){
+        hour++;
+    }
+    else if (task=="homework")
+    {
+        minute = minute+30;
+    }
+    else if (task=="makeBed")
+    {
+        minute = minute+10;
+    }
+    
+    
+
+
+}
+
+int main(){
+
+    return 0;
+}
