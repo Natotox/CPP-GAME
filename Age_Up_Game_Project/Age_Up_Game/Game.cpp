84b7da2080f67d4a13edf91d219e462dae760b18
Natalie Mills <natomills63@gmail.com>
2023-12-06 22:29:08 -0400

    Update Game.cpp
    Introduced update time and wake up functions

--- Age_Up_Game_Project/Age_Up_Game/Game.cpp
+++ Age_Up_Game_Project/Age_Up_Game/Game.cpp
@@ -1,28 +1,51 @@
 #include<iostream>
 using namespace std;
 
-void Update_Time(string task){
-
+struct Time{
     int hour, minute;
+};
 
-    if(task=="class"){
-        hour++;
-    }
-    else if (task=="homework")
-    {
-        minute = minute+30;
-    }
-    else if (task=="makeBed")
-    {
-        minute = minute+10;
-    }
+struct Class{
+    Time duration;
+};
+
+Time update_time(Time current_time, Time add_time){
+    Time updTime;
+    updTime.hour = current_time.hour + add_time.hour;
+    updTime.minute = current_time.minute +  add_time.minute;
+
+    return updTime;
+};
+
+Time wake_up(){
+    Time wuTime;
+    wuTime.hour = 6;
+    wuTime.minute = 0;
     
-    
-
-
-}
+    return wuTime;
+};
 
 int main(){
+    Class math, english;
+    math.duration.hour = 1;
+    math.duration.minute = 0;
+    english.duration.hour = 1;
+    english.duration.minute = 0;
 
+    Time cTime;
+    cTime.hour = 9;
+    cTime.minute = 30;
+    cout << "Time before wake up function is: " << cTime.hour << ":" << cTime.minute << endl;
+
+    cTime = wake_up();
+    cout << "Time after wake up function is: " << cTime.hour << ":" << cTime.minute << endl;
+
+    cout << "Time before Math class is: " << cTime.hour << ":" << cTime.minute << endl;
+
+    cTime = update_time(cTime, math.duration);
+    cout << "Time after Math class is: " << cTime.hour << ":" << cTime.minute << endl;
+
+    cTime = update_time(cTime, english.duration);
+    cout << "Time after English class is: " << cTime.hour << ":" << cTime.minute << endl;
     return 0;
 }
